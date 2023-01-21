5b0c2e8c8df777a7efda84dcea52dfcb7ef24f57
Yanto Iswanto <yantoiswanto18@gmail.com>
2023-01-21 08:27:18 +0700

    add project, slicing splash page,get started page

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:airplane/ui/pages/get_started_page.dart';
+import 'package:airplane/ui/pages/splash_page.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      routes: {
+        '/': (context) => SplashPage(),
+        '/get-started': (context) => GetStartedPage(),
+      },
+    );
+  }
+}
---
+++ lib/ui/pages/get_started_page.dart
@@ -0,0 +1,74 @@
+import 'package:airplane/main.dart';
+import 'package:flutter/material.dart';
+import 'package:airplane/shared/theme.dart';
+
+void main() => runApp(GetStartedPage());
+
+class GetStartedPage extends StatelessWidget {
+  const GetStartedPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(children: [
+        Container(
+          width: double.infinity,
+          height: double.infinity,
+          decoration: BoxDecoration(
+            image: DecorationImage(
+              image: AssetImage(
+                'assets/image_get_started.png',
+              ),
+            ),
+          ),
+        ),
+        Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: [
+              Text(
+                'Fly Like a Bird',
+                style: whiteTextStyle.copyWith(
+                  fontSize: 32,
+                  fontWeight: semiBold,
+                ),
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              Text(
+                'Explore new world with us and let yourself get an amazing experiences',
+                style: whiteTextStyle.copyWith(
+                  fontSize: 16,
+                  fontWeight: light,
+                ),
+                textAlign: TextAlign.center,
+              ),
+              Container(
+                width: 220,
+                height: 55,
+                margin: EdgeInsets.only(top: 50, bottom: 80),
+                child: TextButton(
+                  onPressed: (() {}),
+                  style: TextButton.styleFrom(
+                    backgroundColor: kPrimaryColor,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(17),
+                    ),
+                  ),
+                  child: Text(
+                    'Get Started',
+                    style: whiteTextStyle.copyWith(
+                      fontSize: 18,
+                      fontWeight: medium,
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        )
+      ]),
+    );
+  }
+}
